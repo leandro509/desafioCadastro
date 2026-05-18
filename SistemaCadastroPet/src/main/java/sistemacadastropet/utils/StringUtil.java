1bcc96a5e4cfe64647f6a587e96d1c3896b37b04
leandro509 <leandrogabriel509@gmail.com>
2026-05-18 12:12:06 -0300

    atualizacao arquivo restantes

---
+++ SistemaCadastroPet/src/main/java/sistemacadastropet/utils/StringUtil.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemacadastropet.utils;
+
+/**
+ *
+ * @author leandro
+ */
+public class StringUtil {
+    public static String primeiraLetraMaiuscula(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
+    }
+}
